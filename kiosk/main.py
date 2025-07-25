acf58cad7676c1a20437b1b6c7c34ed4dca65c32
m3211n <serega.ga@gmail.com>
2025-07-25 17:29:47 +0200

    removed cursor function and testing workflow

--- kiosk/main.py
+++ kiosk/main.py
@@ -2,47 +2,35 @@ import subprocess
 from PIL import Image, ImageDraw, ImageFont
 import struct
 
-def hide_cursor():
-    subprocess.run("setterm -cursor off | tee /dev/tty0 > /dev/null", shell=True, check=True)
+WIDTH = 1920
+HEIGHT = 1200
+TEXT = "Hello Pi"
 
-def show_cursor():
-    subprocess.run("setterm -cursor on | tee /dev/tty0 > /dev/null", shell=True, check=True)
+img = Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 255))
+draw = ImageDraw.Draw(img)
 
 try:
-    hide_cursor()
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 64)
+except:
+    font = ImageFont.load_default()
 
-    WIDTH = 1920
-    HEIGHT = 1200
-    TEXT = "Hello Pi"
+bbox = draw.textbbox((0, 0), TEXT, font=font)
+text_width = bbox[2] - bbox[0]
+text_height = bbox[3] - bbox[1]
+text_pos = ((WIDTH - text_width) // 2, (HEIGHT - text_height) // 2)
+draw.text(text_pos, TEXT, font=font, fill=(255, 255, 255))
 
-    img = Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 255))
-    draw = ImageDraw.Draw(img)
+def rgb888_to_rgb565(r, g, b):
+    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
 
-    try:
-        font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", 64)
-    except:
-        font = ImageFont.load_default()
+buffer = bytearray()
+pixels = img.load()
 
-    bbox = draw.textbbox((0, 0), TEXT, font=font)
-    text_width = bbox[2] - bbox[0]
-    text_height = bbox[3] - bbox[1]
-    text_pos = ((WIDTH - text_width) // 2, (HEIGHT - text_height) // 2)
-    draw.text(text_pos, TEXT, font=font, fill=(255, 255, 255))
+for y in range(HEIGHT):
+    for x in range(WIDTH):
+        r, g, b = pixels[x, y]
+        rgb565 = rgb888_to_rgb565(r, g, b)
+        buffer += struct.pack("<H", rgb565)
 
-    def rgb888_to_rgb565(r, g, b):
-        return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-
-    buffer = bytearray()
-    pixels = img.load()
-
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            r, g, b = pixels[x, y]
-            rgb565 = rgb888_to_rgb565(r, g, b)
-            buffer += struct.pack("<H", rgb565)
-
-    with open("/dev/fb0", "wb") as f:
-        f.write(buffer)
-
-finally:
-    show_cursor()
+with open("/dev/fb0", "wb") as f:
+    f.write(buffer)
