fa7cd9014447fa8837d4a3538b047026e5e9278b
m3211n <serega.ga@gmail.com>
2025-07-26 21:23:45 +0200

    color test

--- main.py
+++ main.py
@@ -12,34 +12,25 @@ def rgb888_to_rgb565(r, g, b):
     return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
 
 def draw_test_colors():
-    """Draw test pattern with different color combinations to debug"""
     buffer = bytearray()
     
     for y in range(HEIGHT):
         for x in range(WIDTH):
             if x < WIDTH // 6:
-                # Pure Red
                 r, g, b = 255, 0, 0
             elif x < WIDTH // 3:
-                # Pure Blue  
                 r, g, b = 0, 0, 255
             elif x < WIDTH // 2:
-                # Pure Green
                 r, g, b = 0, 255, 0
             elif x < 2 * WIDTH // 3:
-                # Cyan (Green + Blue)
                 r, g, b = 0, 255, 255
             elif x < 5 * WIDTH // 6:
-                # Magenta (Red + Blue)
                 r, g, b = 255, 0, 255
             else:
-                # Yellow (Red + Green)
                 r, g, b = 255, 255, 0
             
-            # Try different bit layout - maybe not standard RGB565
-            # Your framebuffer might need a different format entirely
             rgb565 = ((g & 0xF8) << 8) | ((r & 0xFC) << 3) | ((b & 0xF8) >> 3)
-            buffer += struct.pack("<H", rgb565)  # little-endian with GRB
+            buffer += struct.pack("<H", rgb565)
 
     with open("/dev/fb0", "wb") as f:
         f.write(buffer)
@@ -52,9 +43,8 @@ def draw_to_framebuffer(image: Image.Image):
     for y in range(HEIGHT):
         for x in range(WIDTH):
             r, g, b = pixels[y, x]
-            # Use exact same format as working test
             rgb565 = ((g & 0xF8) << 8) | ((r & 0xFC) << 3) | ((b & 0xF8) >> 3)
-            buffer += struct.pack("<H", rgb565)  # little-endian with GRB
+            buffer += struct.pack("<H", rgb565)
 
     with open("/dev/fb0", "wb") as f:  
         f.write(buffer)
@@ -68,10 +58,6 @@ def fetch_cat_image():
         return Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 0))  # fallback
 
 async def update_loop():
-    # Test first to confirm colors work
-    draw_test_colors()
-    await asyncio.sleep(5)
-    
     while True:
         cat = fetch_cat_image()
         draw_to_framebuffer(cat)
