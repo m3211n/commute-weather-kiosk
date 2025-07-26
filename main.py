71ab0ec7e7a59520c80472b1ca119542e5c501de
m3211n <serega.ga@gmail.com>
2025-07-26 20:26:26 +0200

    testing colors

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ HEIGHT = 600
 
 def draw_to_framebuffer(image: Image.Image):
     arr = np.array(image.convert("RGB"))
-    r = arr[:, :, 0] >> 3
+
+    # BGR channel extraction (swap red and blue)
+    b = arr[:, :, 0] >> 3  # originally red
     g = arr[:, :, 1] >> 2
-    b = arr[:, :, 2] >> 3
-    rgb565 = (r << 11) | (g << 5) | b
-    buffer = rgb565.astype('<u2').tobytes()
+    r = arr[:, :, 2] >> 3  # originally blue
+
+    rgb565 = (b << 11) | (g << 5) | r  # BGR565
+
+    # Write as big-endian (high byte first)
+    buffer = rgb565.astype('>u2').tobytes()
+
     with open("/dev/fb0", "wb") as f:
         f.write(buffer)
 
