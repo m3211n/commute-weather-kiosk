ded9ca49fe03d73c25448e41d7f07b01792d8fe9
m3211n <serega.ga@gmail.com>
2025-07-26 15:01:29 +0200

    some refactoring

---
+++ app/widget.py
@@ -0,0 +1,8 @@
+from app.screen import Screen, WIDTH, HEIGHT, GRID_UNIT, blk
+from PIL import Image, ImageDraw, ImageFont
+
+class Widget():
+    def __init__(self, position=(0, 0), size=(blk(10), blk(10)), container=True):
+        
+        pass
+
--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-from PIL import Image, ImageDraw, ImageFont
-import struct
+from PIL import ImageFont
+from app.screen import Screen, WIDTH, HEIGHT
 
-WIDTH = 1920
-HEIGHT = 1200
+TESTING = True
+
 TEXT = "FUCK OFF HAHAHA!!!"
 
-img = Image.new("RGB", (WIDTH, HEIGHT), (0, 0, 0))
-draw = ImageDraw.Draw(img)
+screen = Screen()
 
-try:
-    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 64)
-except:
-    font = ImageFont.load_default()
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 64)
 
-bbox = draw.textbbox((0, 0), TEXT, font=font)
+bbox = screen.canvas.textbbox((0, 0), TEXT, font=font)
 text_width = bbox[2] - bbox[0]
 text_height = bbox[3] - bbox[1]
 text_pos = ((WIDTH - text_width) // 2, (HEIGHT - text_height) // 2)
-draw.text(text_pos, TEXT, font=font, fill=(255, 0, 0))
+screen.canvas.text(text_pos, TEXT, font=font, fill=(255, 0, 0))
 
-def rgb888_to_rgb565(r, g, b):
-    return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3)
-
-buffer = bytearray()
-pixels = img.load()
-
-for y in range(HEIGHT):
-    for x in range(WIDTH):
-        r, g, b = pixels[x, y]
-        rgb565 = rgb888_to_rgb565(r, g, b)
-        buffer += struct.pack("<H", rgb565)
-
-with open("/dev/fb0", "wb") as f:
-    f.write(buffer)
+if TESTING:
+    screen.image.save("__preview/output.png") 
+else:
+    screen.output()
