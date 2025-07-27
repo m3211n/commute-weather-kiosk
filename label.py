7985d98ada613d0cdeab9945aee6a886c14fd00b
m3211n <serega.ga@gmail.com>
2025-07-27 02:00:57 +0200

    new text label

--- label.py
+++ label.py
@@ -12,30 +12,29 @@ def rgb888_to_rgb565_numpy(image):
     rgb565 = ((r >> 3) << 11) | ((g >> 2) << 5) | (b >> 3)
     return rgb565.astype('<u2').tobytes()
 
-
 class Label:
-    def __init__(self, x, y, width, height, font_size=24, color="white", bg="black"):
+    def __init__(self, x, y, text="...", font_size=24, color="white", bg="black"):
+        self.font_size = font_size
+        self.font = ImageFont.truetype(FONT_PATH, self.font_size)
+        self.text(text)
         self.x = x
         self.y = y
-        self.width = width
-        self.height = height
-        self.font_size = font_size
         self.color = color
         self.bg = bg
-        self.text = ""
-        self.font = ImageFont.truetype(FONT_PATH, self.font_size)
+
+    async def text(self, new_text):
+        self._text = new_text
+        bbox = self.font.getbbox(self.text) # fallback text if empty
+        self.width = bbox[2] - bbox[0]
+        self.height = bbox[3] - bbox[1]
+        image = self._render(new_text)
+        await self._write_to_framebuffer(image)
 
     def _render(self, new_text):
         image = Image.new("RGB", (self.width, self.height), self.bg)
         draw = ImageDraw.Draw(image)
         draw.text((0, 0), new_text, fill=self.color, font=self.font)
         return image
-
-    async def update(self, new_text):
-        if new_text != self.text:
-            self.text = new_text
-            image = self._render(new_text)
-            await self._write_to_framebuffer(image)
 
     async def _write_to_framebuffer(self, image):
         buf = await asyncio.to_thread(rgb888_to_rgb565_numpy, image)
--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import asyncio
 from label import Label
 from datetime import datetime
 
-label = Label(x=560, y=500, width=800, height=200, font_size=120)
+label = Label(x=560, y=500, font_size=120)
 
 async def main():
     while True:
-        now = datetime.now().strftime("%H:%M:%S")
-        await label.update(now)
+        await label.text(datetime.now().strftime("%H:%M:%S"))
         await asyncio.sleep(1)
 
 if __name__ == "__main__":
