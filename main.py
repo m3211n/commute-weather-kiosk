27f7a00be572952b424be2a8d61cdf2092509fca
m3211n <serega.ga@gmail.com>
2025-07-28 22:23:37 +0200

    test

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 from PIL import Image, ImageDraw
 # from datetime import datetime
 from screen import Screen
-from shared.fonts import Fonts
+from shared.styles import Fonts
 
 class Widget:
     def __init__(self, name, position=(0, 0), size=(100, 100), bgcolor=(0, 0, 0)):
@@ -18,17 +18,22 @@ class Widget:
         return self.image
     
     def clean(self):
-        self._draw_context.rectangle([0, 0, self.w, self.h], fill=self.bgcolor)
+        self._draw_context.rounded_rectangle([0, 0, self.w, self.h], radius=8, fill=self.bgcolor)
 
     def text(self, *args, **kwargs):
         self._draw_context.text(*args, **kwargs)
+    
+    def add_image(self, path, coordinates):
+        im = Image.open(path)
+        box = coordinates[0], coordinates[1], coordinates[0] + im.width, coordinates[1] + im.height
+        self.image.paste(im, box)
 
 widgets = {
     Widget("Clock", (8, 8), (948, 560), (28, 28, 28)),
     Widget("Trains_1", (8, 576), (948, 200), (28, 28, 28)),
     Widget("Trains_2", (8, 784), (948, 200), (28, 28, 28)),
     Widget("Busses_1", (8, 992), (948, 200), (28, 28, 28)),
-    Widget("Vaginas", (964, 8), (948, 1184), (28, 28, 28))
+    Widget("Weather", (964, 8), (948, 1184), (28, 28, 28))
 }
 
 
@@ -36,11 +41,12 @@ async def main():
     with Screen() as s:
         s.add(widgets)
         await s.clear()
+        s.widgets["Weather"].add_image("shared/weather_icons/cloudy.png", (200, 200))
         while True:
             for widget in s.widgets.values():
                 widget.clean()
                 widget.text((8, 8), f"{widget.name}", font=Fonts.title, fill=(255, 255, 255))
-                widget.text((8, 20), f"{random.randint(1000, 9999)}", font=Fonts.value_small, fill=(255, 255, 255))      
+                widget.text((8, 20), f"{random.randint(1000, 9999)}", font=Fonts.value, fill=(255, 255, 255))      
             await s.refresh_all()
             await asyncio.sleep(0.1)
 
--- shared/styles.py
+++ shared/styles.py
@@ -1,11 +1,16 @@
 from PIL import ImageFont
 
-
 DejaVuSans = lambda size: ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", size)
 Mono = lambda size: ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf", size) 
 
 class Fonts:
-    value_small = DejaVuSans(80)
+    value = DejaVuSans(80)
     title = Mono(40)
-    weather_temp_large = DejaVuSans(120)
+    weather_today = DejaVuSans(120)
     clock = DejaVuSans(240)
+
+class Colors:
+    panel_bg = (32, 32, 32)
+    title = (88, 88, 88)
+    departure_times = (216, 216, 0)
+    default = (216, 216, 216)
