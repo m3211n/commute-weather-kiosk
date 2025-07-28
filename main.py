a44646daa37033eda5a2687f73a268ee66449885
m3211n <serega.ga@gmail.com>
2025-07-28 22:29:43 +0200

    test

--- main.py
+++ main.py
@@ -21,6 +21,8 @@ class Widget:
         self._draw_context.rounded_rectangle([0, 0, self.w, self.h], radius=8, fill=self.bgcolor)
 
     def text(self, *args, **kwargs):
+        box = self._draw_context.textbbox(*args, **kwargs)
+        self._draw_context.rectangle(box, fill=self.bgcolor)
         self._draw_context.text(*args, **kwargs)
     
     def add_image(self, path, coordinates):
@@ -44,9 +46,8 @@ async def main():
         s.widgets["Weather"].add_image("shared/weather_icons/cloudy.png", (200, 200))
         while True:
             for widget in s.widgets.values():
-                widget.clean()
                 widget.text((8, 8), f"{widget.name}", font=Fonts.title, fill=(255, 255, 255))
-                widget.text((8, 20), f"{random.randint(1000, 9999)}", font=Fonts.value, fill=(255, 255, 255))      
+                widget.text((8, 32), f"{random.randint(1000, 9999)}", font=Fonts.value, fill=(255, 255, 255))      
             await s.refresh_all()
             await asyncio.sleep(0.1)
 
