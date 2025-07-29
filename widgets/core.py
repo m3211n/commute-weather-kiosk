58a6acc43248fccc7d2773aad274e75f75f865c0
m3211n <serega.ga@gmail.com>
2025-07-29 16:05:59 +0200

    test

--- widgets/core.py
+++ widgets/core.py
@@ -1,48 +1,17 @@
 from PIL import Image, ImageDraw
 from shared.styles import Fonts, Colors
+from typing import List
 
 
-class Widget:
-    """Generic widget class"""
-    def __init__(
-            self,
-            position=(0, 0),
-            size=(100, 100),
-            bgcolor=Colors.panel_bg,
-            content=[]):
-
-        self.position = position
-        self.size = size
-        self.bgcolor = bgcolor
-        self.content = content
-        self.image = Image.new("RGB", self.size)
-        self._draw_context = ImageDraw.Draw(self.image)
-
-    def is_up_to_date(self) -> bool:
-        if any(item.callback() for item in self.content):
-            return True
-        return False
-
-    def render(self) -> bool:
-        if self.is_up_to_date():
-            self._draw_context.rounded_rectangle(
-                [(0, 0), self.size],
-                radius=8,
-                fill=self.bgcolor
-            )
-            for item in self.content:
-                if isinstance(item, Label):
-                    self._draw_context.text(**item.__dict__)
-            return True
-        return False
+class Content():
+    """Generic class for dynamically updated content"""
+    def __init__(self):
+        self.callback = None
 
 
-class Label:
+class Label(Content):
     """Generic label class.
     Uses the same attributes as PIL.ImageDraw.Draw.text"""
-
-    def _default_callback():
-        return False
 
     def __init__(
             self,
@@ -58,3 +27,38 @@ class Label:
         self.font = font
         self.anchor = anchor
         self.callback = callback
+
+
+class Widget:
+    """Generic widget class"""
+    def __init__(
+            self,
+            position=(0, 0),
+            size=(100, 100),
+            bgcolor=Colors.panel_bg,
+            content: List[Content] = []):
+
+        self.position = position
+        self.size = size
+        self.bgcolor = bgcolor
+        self.content = content
+        self.image = Image.new("RGB", self.size)
+        self._draw_context = ImageDraw.Draw(self.image)
+
+    def _dirty(self) -> bool:
+        if any(item.callback() for item in self.content):
+            return True
+        return False
+
+    def render(self) -> bool:
+        if self._dirty():
+            self._draw_context.rounded_rectangle(
+                [(0, 0), self.size],
+                radius=8,
+                fill=self.bgcolor
+            )
+            for item in self.content:
+                if isinstance(item, Label):
+                    self._draw_context.text(**item.__dict__)
+            return True
+        return False
