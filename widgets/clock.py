9c042b07c590ce9fb879e824e00c6b20deffb6df
m3211n <serega.ga@gmail.com>
2025-07-29 18:55:54 +0200

    fix

--- widgets/clock.py
+++ widgets/clock.py
@@ -33,5 +33,5 @@ class Clock(Widget):
         return False
 
     async def render(self):
-        self._draw_context.text(**self.labelTime)
-        self._draw_context.text(**self.labelDate)
+        self._draw_context.text(*self.labelTime)
+        self._draw_context.text(*self.labelDate)
