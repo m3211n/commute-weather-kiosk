086b639b94ff73dc2047aa6328de8e7a7b8dd4e1
m3211n <serega.ga@gmail.com>
2025-07-29 02:57:54 +0200

    fix

--- widgets/clock.py
+++ widgets/clock.py
@@ -11,8 +11,8 @@ class Clock(Widget):
     def __init__(self, interval=60):
         super().__init__("Clock", interval=interval, position=(8, 8), size=(948, 472))
         self.content = {
-            "time": Label("--:--", font=Fonts.clock, anchor="mb", fill=Colors.default),
-            "date": Label("Today", font=Fonts.title, anchor="mt", position=(0, 16), fill=Colors.title)
+            "time": Label("--:--", font=Fonts.clock, anchor="mb", position=(474, 236), fill=Colors.default),
+            "date": Label("Today", font=Fonts.title, anchor="mt", position=(474, 252), fill=Colors.title)
         }
     
     async def callback(self):
