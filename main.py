29756336c22bd9ec5d02f56793ecbf4fec733e2e
m3211n <serega.ga@gmail.com>
2025-07-29 12:07:22 +0200

    loggin as info

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from widgets import Clock
 # from datetime import datetime
 from screen import Screen
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 widgets = {
     Clock(60)
@@ -18,7 +18,7 @@ async def main():
         while True:
             # on = time.time()
             response = await s.refresh_all()
-            logging.debug(" Screen loop reported: %s", response)
+            logging.info(" Screen loop reported: %s", response)
             await asyncio.sleep(5)
 
 if __name__ == "__main__":
