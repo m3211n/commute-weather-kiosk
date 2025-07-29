192efbca5e7b9d7fdfef17bbd0a8b8dfe4a1059b
m3211n <serega.ga@gmail.com>
2025-07-29 20:56:00 +0200

    test

--- widgets/clock.py
+++ widgets/clock.py
@@ -10,12 +10,12 @@ class Clock(Widget):
     def __init__(self):
         super().__init__(position=(8, 8), size=(948, 548))
         self.labelTime = Label(
-            xy=(474, 274),
+            xy=(474, 338),
             font=Fonts.CLOCK,
             anchor="mb"
         )
         self.labelDate = Label(
-            xy=(474, 298),
+            xy=(474, 360),
             fill=Colors.SECONDARY,
             font=Fonts.TITLE,
             anchor="mt"
