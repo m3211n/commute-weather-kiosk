424b09e1467fb2e394eef7769f950bbd0208ee26
m3211n <serega.ga@gmail.com>
2025-07-29 03:07:14 +0200

    fix

--- widgets/base.py
+++ widgets/base.py
@@ -15,16 +15,6 @@ class Widget:
         self._draw_context = ImageDraw.Draw(self.image)
         self._draw_context.rounded_rectangle([(0, 0), self.size], radius=8, fill=self.bgcolor)
     
-    def add_text(self, *args, **kwargs):
-        box = self._draw_context.textbbox(args[0], args[1], font=kwargs.get("font"))
-        self._draw_context.rectangle(box, fill=self.bgcolor)
-        self._draw_context.text(*args, **kwargs)
-    
-    def add_image(self, path, coordinates):
-        im = Image.open(path)
-        box = coordinates[0], coordinates[1], coordinates[0] + im.width, coordinates[1] + im.height
-        self.image.paste(im, box, mask=im.split()[3])
-    
     async def callback(self):
         pass
 
@@ -45,7 +35,6 @@ class Label:
     def __init__(self, text="Label", font=Fonts.value, position=(0, 0), anchor="la", fill=Colors.default, bg=Colors.panel_bg):
         self.text = text
         self.font = font
-        self.bbox = self.get_bbox()
         self.size = self.bbox[2] - self.bbox[0], self.bbox[3] - self.bbox[1]
         self.position = position
         self.fill = fill
@@ -53,12 +42,12 @@ class Label:
         self.image = Image.new("RGB", self.size, self.bg)
         self.context = ImageDraw.Draw(self.image)
         self.anchor = anchor
-
-    def get_bbox(self):
+    
+    @property
+    def bbox(self):
         return self.font.getbbox(self.text)
 
     async def update(self, new_text):
         self.text = new_text
-        self.bbox = self.get_bbox()
-        self.image.paste((0, 0, 0, 0), self.bbox)
+        self.image.paste((0, 0, 0), self.bbox)
         self.context.text((0, 0), self.text, self.fill, self.font, self.anchor)
