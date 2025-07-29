e3a79c33ff689de8b5f6f0b43cc895a6b2eb8537
m3211n <serega.ga@gmail.com>
2025-07-29 16:17:22 +0200

    refactrorio

--- widgets/clock.py
+++ widgets/clock.py
@@ -6,32 +6,31 @@ TIME_FORMAT = "%H:%M"
 DATE_FORMAT = "%A, %B %d, %Y"
 
 
-def _callback(label: Label, f):
-    new_text = datetime.now().strftime(f)
-    if not label.text == new_text:
-        label.text = new_text
-        return True
-    return False
+class Clock(Widget):
+    def __init__(self):
+        self.position = (8, 8),
+        self.size = (948, 472),
 
+        def _callback(label: Label, f):
+            new_text = datetime.now().strftime(f)
+            if not label.text == new_text:
+                label.text = new_text
+                return True
+            return False
 
-labelTime = Label(
-    xy=(474, 268),
-    fill=Colors.default,
-    font=Fonts.clock,
-    anchor="mb",
-    callback=lambda: _callback(labelTime, TIME_FORMAT)
-)
+        labelTime = Label(
+                xy=(474, 268),
+                fill=Colors.default,
+                font=Fonts.clock,
+                anchor="mb",
+                callback=lambda: _callback(labelTime, TIME_FORMAT)
+        )
+        labelDate = Label(
+                xy=(474, 300),
+                fill=Colors.title,
+                font=Fonts.title,
+                anchor="mt",
+                callback=lambda: _callback(labelDate, DATE_FORMAT)
+        )
 
-labelDate = Label(
-    xy=(474, 300),
-    fill=Colors.title,
-    font=Fonts.title,
-    anchor="mt",
-    callback=lambda: _callback(labelDate, DATE_FORMAT)
-)
-
-clock = Widget(
-    position=(8, 8),
-    size=(948, 472),
-    content=[labelTime, labelDate]
-    )
+        self.content = [labelTime, labelDate]
