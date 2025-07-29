deef578459bdef89dd134fa37ba08963af28824d
m3211n <serega.ga@gmail.com>
2025-07-29 03:39:46 +0200

    fixxxx

--- widgets/base.py
+++ widgets/base.py
@@ -10,7 +10,7 @@ class Widget:
         self.x, self.y = position
         self.size = size
         self.bgcolor = bgcolor
-        self.content = {}
+        self.text_content = {}
         self.image = Image.new("RGB", self.size)
         self._draw_context = ImageDraw.Draw(self.image)
         self._draw_context.rounded_rectangle([(0, 0), self.size], radius=8, fill=self.bgcolor)
@@ -25,29 +25,24 @@ class Widget:
     async def update(self):
         if not self.dirty:
             await self.callback()
-        for item in self.content.values():
-            self.image.paste(item.image, item.position)
+        for item in self.text_content.values():
+            self._draw_context.text(*item)
         await asyncio.sleep(self._interval)
         self.dirty = True
 
 
 class Label:
-    def __init__(self, text="Label", font=Fonts.value, position=(0, 0), anchor="la", fill=Colors.default, bg=Colors.panel_bg):
-        self.text = text
+    def __init__(self, xy=(0, 0), text="Label", fill=Colors.default, font=Fonts.value, anchor="la"):
+        self._text = text
         self.font = font
-        self.size = self.bbox[2] - self.bbox[0], self.bbox[3] - self.bbox[1]
-        self.position = position
+        self.xy = xy
         self.fill = fill
-        self.bg = bg
-        self.image = Image.new("RGB", self.size, self.bg)
-        self.context = ImageDraw.Draw(self.image)
         self.anchor = anchor
-    
-    @property
-    def bbox(self):
-        return self.font.getbbox(self.text)
 
-    async def update(self, new_text):
-        self.text = new_text
-        self.image.paste(self.bg, self.bbox)
-        self.context.text((0, 0), self.text, self.fill, self.font, self.anchor)
+    @property
+    def text(self):
+        return self._text
+
+    @text.setter
+    def text(self, new_text):
+        self._text = new_text
--- widgets/clock.py
+++ widgets/clock.py
@@ -10,11 +10,11 @@ time_str = lambda format: datetime.now().strftime(format)
 class Clock(Widget):
     def __init__(self, interval=60):
         super().__init__("Clock", interval=interval, position=(8, 8), size=(948, 472))
-        self.content = {
-            "time": Label("--:--", font=Fonts.clock, anchor="mb", position=(474, 236), fill=Colors.default),
-            "date": Label("Today", font=Fonts.title, anchor="mt", position=(474, 252), fill=Colors.title)
+        self.text_content = {
+            "time": Label(xy=(474, 236), text="--:--", fill=Colors.default, font=Fonts.clock, anchor="mb"),
+            "date": Label(xy=(474, 252), text="Today", fill=Colors.title, font=Fonts.title, anchor="mt")
         }
     
     async def callback(self):
-        await self.content["time"].update(time_str(TIME))
-        await self.content["date"].update(time_str(DATE))
+        self.text_content["time"].text = time_str(TIME)
+        self.text_content["date"].text = time_str(DATE)
