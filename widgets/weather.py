15d2fd2a631178b3ee135737d050bdd653a2a9f7
m3211n <serega.ga@gmail.com>
2025-08-01 00:48:29 +0200

    test

--- widgets/weather.py
+++ widgets/weather.py
@@ -12,13 +12,21 @@ class Weather(Widget):
             fill=Colors.DEFAULT,
             anchor="lt"
         )
+        self.labelConditions = Label(
+            xy=(90, 120),
+            font=Fonts.WEATHER_TODAY,
+            fill=Colors.TITLE,
+            anchor="lt"
+        )
         self.timeout = timeout
         self._next_update = Tools.time()
 
     async def update_content(self) -> bool:
         self.labelTemperature.text = "24°C"
+        self.labelConditions.text = "Clear"
         return self._update_timeout()
 
     async def render(self):
         self._clear()
         await self.labelTemperature.render_at(self.image)
+        await self.labelConditions.render_at(self.image)
